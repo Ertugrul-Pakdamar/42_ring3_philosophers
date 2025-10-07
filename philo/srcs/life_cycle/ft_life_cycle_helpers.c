8e3bd8bc3295f9cfc9295495453180c7fce4f4a7
ertugrul-pakdamar <ertu.pakdamar@gmail.com>
2025-10-07 21:18:38 +0300

    update: modify file headers with new timestamps

--- philo/srcs/life_cycle/ft_life_cycle_helpers.c
+++ philo/srcs/life_cycle/ft_life_cycle_helpers.c
@@ -6,31 +6,31 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 18:20:00 by epakdama          #+#    #+#             */
-/*   Updated: 2025/10/07 21:14:27 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/10/07 21:17:23 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_philo.h"
 
-int	ft_one_philo_cycle(pthread_mutex_t *right, t_data *data, t_philo *philo)
+int	ft_one_philo_cycle(int idx, t_data *data, t_philo *philo)
 {
-	pthread_mutex_lock(right);
+	pthread_mutex_lock(&data->philos[idx]->right_fork);
 	ft_print_status(data, philo, FORK);
 	while (!ft_get_die(data))
 		usleep(1000);
-	pthread_mutex_unlock(right);
+	pthread_mutex_unlock(&data->philos[idx]->right_fork);
 	return (0);
 }
 
-int	ft_multi_philo_cycle(pthread_mutex_t *right, pthread_mutex_t *left,
-		t_data *data, t_philo *philo)
+int	ft_multi_philo_cycle(int idx, pthread_mutex_t *left, t_data *data,
+		t_philo *philo)
 {
 	if (philo->id % 2 == 0)
 	{
-		pthread_mutex_lock(right);
+		pthread_mutex_lock(&data->philos[idx]->right_fork);
 		ft_print_status(data, philo, FORK);
 		if (ft_get_die(data))
-			return (pthread_mutex_unlock(right));
+			return (pthread_mutex_unlock(&data->philos[idx]->right_fork));
 		pthread_mutex_lock(left);
 		ft_print_status(data, philo, FORK);
 	}
@@ -41,7 +41,7 @@ int	ft_multi_philo_cycle(pthread_mutex_t *right, pthread_mutex_t *left,
 		ft_print_status(data, philo, FORK);
 		if (ft_get_die(data))
 			return (pthread_mutex_unlock(left));
-		pthread_mutex_lock(right);
+		pthread_mutex_lock(&data->philos[idx]->right_fork);
 		ft_print_status(data, philo, FORK);
 	}
 	return (1);
@@ -50,20 +50,18 @@ int	ft_multi_philo_cycle(pthread_mutex_t *right, pthread_mutex_t *left,
 int	ft_prepare_to_eat(t_data *data, t_philo *philo)
 {
 	int				idx;
-	pthread_mutex_t	*right;
 	pthread_mutex_t	*left;
 
 	if (ft_get_die(data))
 		return (0);
 	idx = philo->id - 1;
-	right = &data->philos[idx]->right_fork;
+	if (data->philo_count == 1)
+	{
+		return (ft_one_philo_cycle(idx, data, philo));
+	}
 	if (idx == 0)
 		left = &data->philos[data->philo_count - 1]->right_fork;
 	else
 		left = &data->philos[idx - 1]->right_fork;
-	if (data->philo_count == 1)
-	{
-		return (ft_one_philo_cycle(right, data, philo));
-	}
-	return (ft_multi_philo_cycle(right, left, data, philo));
+	return (ft_multi_philo_cycle(idx, left, data, philo));
 }
