bb79cafe6d2cc7c4945c1cb51a59deb8831869ed
ertugrul-pakdamar <ertu.pakdamar@gmail.com>
2025-10-08 19:28:05 +0300

    I finally did it

--- philo/srcs/threads/ft_create_threads.c
+++ philo/srcs/threads/ft_create_threads.c
@@ -6,7 +6,7 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 10:25:54 by epakdama          #+#    #+#             */
-/*   Updated: 2025/10/08 11:21:01 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/10/08 19:27:10 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,19 @@ void	ft_create_threads(t_data *data)
 	pthread_mutex_init(&data->die_mutex, NULL);
 	pthread_mutex_init(&data->meals_required_mutex, NULL);
 	pthread_mutex_init(&data->time_mutex, NULL);
+	i = 0;
+	while (i < data->philo_count)
+	{
+		philo = data->philos[i];
+		pthread_mutex_init(&philo->right_fork, NULL);
+		i++;
+	}
 	pthread_create(&data->death_thread, NULL, ft_death_thread, data);
 	data->start_time = ft_get_time(data);
 	i = 0;
 	while (i < data->philo_count)
 	{
 		philo = data->philos[i];
-		pthread_mutex_init(&philo->right_fork, NULL);
 		if (pthread_create(&philo->thread, NULL, ft_life_cycle, philo))
 			break ;
 		i++;
